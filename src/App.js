d31a92fb0b72f8817707ef708238a3f610c69bf8
ABHINAV <debmickey4@gmail.com>
2024-05-17 01:20:49 +0530

    added failsafe routing

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const App = () => (
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        <Route path="*" element={<Feed />} />
       </Routes>
     </Box>
   </BrowserRouter>
